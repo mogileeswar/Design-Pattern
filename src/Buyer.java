f22ccfd879d56a990ca64e7c73af58aeefca4697
Mogileeswar Reddy Morramreddygari (Student) <mmorramr@sundevils.asu.edu>
2022-10-19 21:51:20 -0700

    Updated comments

--- src/Buyer.java
+++ src/Buyer.java
@@ -1,11 +1,10 @@
+//Implementation of the person class
 public class Buyer extends Person {
-
-	private Person person;
-
+	//To show the appropriate items on the menu according to the need of the buyer
 	public void showMenu() {
 
 	}
-
+	// To create a concrete menu according to the product type
 	public ProductMenu CreateProductMenu() {
 		return null;
 	}
