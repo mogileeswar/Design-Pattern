5144dc9598e57a573b8a0c38262b7ce8eb04510d
mogileeswar <112651133+mogileeswar@users.noreply.github.com>
2022-10-20 23:48:31 -0700

    Add files via upload
    updated code and added comments

--- src/ProductMenu.java
+++ src/ProductMenu.java
@@ -1,27 +1,15 @@
+package online_trading;
+
+//This class is used to implement the bridge class
+//To seperateabstract from implementation bridge pattern is used
+//to implement the appropriate menu bridge pattern is used
+//abstract class is the prosuct menu
 public class ProductMenu {
 
-	public void showMenu() {
-
-	}
-
-	public void showAddButton() {
-
-	}
-
-	public void showViewButton() {
-
-	}
-
-	public void showRadioButton() {
-
-	}
-
-	public void showLabels() {
-
-	}
-
-	public void showComboxes() {
-
-	}
-
+	public void showMenu();
+	public void showAddButton();
+	public void showViewButton();
+	public void showRadioButton();
+	public void showLabels();
+	public void showComboxes();
 }
