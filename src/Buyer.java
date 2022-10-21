2d69a610012bb2ae8bcd89ada59786768becc557
mogileeswar <112651133+mogileeswar@users.noreply.github.com>
2022-10-20 23:40:31 -0700

    Add files via upload
    updated code and added comments

--- src/Buyer.java
+++ src/Buyer.java
@@ -1,11 +1,23 @@
-//Implementation of the person class
+package Online_trading;
+
+//Buyer class
+////Implementation of the person class
 public class Buyer extends Person {
+
+	Buyer() {
+		super(ProductMenu);
+	}
 	//To show the appropriate items on the menu according to the need of the buyer
 	public void showMenu() {
-
+			System.out.println("Items on the menu for Buyer...")
 	}
 	// To create a concrete menu according to the product type
 	public ProductMenu CreateProductMenu() {
-		return null;
+		String test = "Produce";
+		if (test.equalsIgnoreCase("Produce")){
+			return new Produce();
+		}
+		return new Meat();
 	}
+
 }
