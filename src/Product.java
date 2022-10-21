4b4557efe2d30f651ef109cefb457031bb18e9db
mogileeswar <112651133+mogileeswar@users.noreply.github.com>
2022-10-20 23:46:39 -0700

    Add files via upload
    updated code and added comments

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,34 @@
 public class Product {
 
+    import java.util.ArrayList;
+    import java.util.Iterator;
+}
+public ClassProductList classProductList;
+public ProductList extends Reminder
+{
+    ArrayList<string> sup = new ArrayList<>();
+    ArrayList<string> setup = new ArrayList<>();
+    Productlist()
+    {
+        sup.add("Beef");
+        sup.add("Onion");
+        sup.add("Tomato");
+        sup.add("Mutton");
+        sup.add("Pork");
+    }
+public ArrayList<String> returnlist(String useName)
+{
+    if(userName.equals("nana"))
+    {
+        setuplist.add("Pork");
+        setuplist.add("Onion");
+        return setuplist;
+    }
+    else if(username.equals("mimi"))
+    {
+        setuplist.add("Tomato");
+        setuplist.add("Onion");
+    }
+    return null;
+}
 }
