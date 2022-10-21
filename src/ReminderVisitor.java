a702fb8410280d16d30199c0a6ecde0fa86f4546
mogileeswar <112651133+mogileeswar@users.noreply.github.com>
2022-10-20 23:49:23 -0700

    Add files via upload
    updated code and added comments

--- src/ReminderVisitor.java
+++ src/ReminderVisitor.java
@@ -1,8 +1,6 @@
 public class ReminderVisitor extends NodeVisitor {
-
+// Implementation of visitor pattern is done here
 	private Reminder m_Reminder;
-
-	private ReminderVisitor.NodeVisitor nodeVisitor;
 
 	private ReminderVisitor.NodeVisitor nodeVisitor;
 
@@ -17,17 +15,22 @@ public class ReminderVisitor extends NodeVisitor {
 	public void visitFacade(Facade facade) {
 
 	}
+	public class ReminderVisitor extends NodeVisitor{
+		String m_Reminder;
+	}
 
 	public class NodeVisitor extends ReminderVisitor {
 
 		private ReminderVisitor reminderVisitor;
 
-		public void visitProduct(Product product) {
-
+		public Reminder visitProduct(ProductList PrdList) {
+			System.out.println("Product visitng...");
+			return PrdList
 		}
 
-		public void visitTrading(Trading trading) {
-
+		public Reminder visitTrading(Trading trading) {
+			System.out.println(" Trade visiting...");
+			return trading;
 		}
 
 		public void visitFacade(Facade facade) {
