5f957e07b0396b96cc8716298e51c12530c5824b
mogileeswar <112651133+mogileeswar@users.noreply.github.com>
2022-10-20 23:44:28 -0700

    Add files via upload
    updated code and added comments

--- src/NodeVisitor.java
+++ src/NodeVisitor.java
@@ -1,15 +1,11 @@
+package online_trading;
+
+//in this class visitor design pattern is implemented
+//to encapsulate the inner implementation nde visitor is used
 public class NodeVisitor {
 
-	public void visitProsuct(Product product) {
-
-	}
-
-	public void visitTrading(Trading trading) {
-
-	}
-
-	public void visitFacade(float facade) {
-
-	}
-
+	import 
+	public void visitProsuct(Product product);
+	public void visitTrading(Trading trading);
+	public void visitFacade(float facade);
 }
