51a881a5e0a5ca7aede88e22d64cff48da9d5faa
mogileeswar <112651133+mogileeswar@users.noreply.github.com>
2022-10-20 23:50:40 -0700

    Add files via upload
    updated code and added comments

---
+++ src/TradingMenu.java
@@ -0,0 +1,14 @@
+package online_trading;
+
+//to get the trading menu
+public class TradingMenu
+{
+    public void AddTrading()
+    {
+        System.out.println("Trade Addition...")
+    }
+    public void viewTrading(Trading trading, Person person)
+    {
+        System.out.println("Trade viewing...")
+    }
+}
