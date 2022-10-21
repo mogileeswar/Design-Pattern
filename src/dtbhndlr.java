742de4f61ea11bfa09031dffc37a8116229b4491
mogileeswar <112651133+mogileeswar@users.noreply.github.com>
2022-10-20 23:42:17 -0700

    Add files via upload
    updated code and added comments

---
+++ src/dtbhndlr.java
@@ -0,0 +1,14 @@
+package online_trading;
+
+import java.uti.Arraylist;
+import java.util.List;
+
+public class dtbhndlr implements Databasehandle {
+    private Map<String, User> map = new Map<>();
+    private status Databasehandler dtbhandle;
+
+    private Databasehandler
+        map.put("nana", new user(UserType.Buyer, "3333"));
+        map.put("mimi", new user(UserType.Buyer, "2222"));
+        map.put("tutu", new user(UserType.Buyer, "1111"));
+}
