549bb63a6baaa88f11cdc2dad7590977df520714
mogileeswar <112651133+mogileeswar@users.noreply.github.com>
2022-10-20 23:43:49 -0700

    Add files via upload
    updated code and added comments

--- src/MeatProductMenu.java
+++ src/MeatProductMenu.java
@@ -1,27 +1,29 @@
+package online_trading;
+//this class is used to extend the meat class
 public class MeatProductMenu extends ProductMenu {
 
-	public void showMenu() {
-
+	public void showMenu()
+	{
+		System.out.println();
 	}
-
-	public void showAddButton() {
-
+	public void showAddButton()
+	{
+		System.out.println();
 	}
-
-	public void showViewButton() {
-
+	public void showViewButton()
+	{
+		System.out.println();
 	}
-
-	public void showRadioButton() {
-
+	public void showRadioButton()
+	{
+		System.out.println();
 	}
-
-	public void showLabels() {
-
+	public void showLabels()
+	{
+		System.out.println();
 	}
-
-	public void shoeComboxes() {
-
+	public void shoeComboxes()
+	{
+		System.out.println();
 	}
-
 }
