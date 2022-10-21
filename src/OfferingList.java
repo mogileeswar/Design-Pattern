3ba1e46898b159de6d4388267c302ea38260ed95
mogileeswar <112651133+mogileeswar@users.noreply.github.com>
2022-10-20 23:45:31 -0700

    Add files via upload
    updated code and added comments

--- src/OfferingList.java
+++ src/OfferingList.java
@@ -1,5 +1,18 @@
-public class OfferingList {
+package online_trading;
 
-	private OfferingIterator offeringIterator;
+//Class Offering list
+import java.util.ArrayList;
+import java.util.Iterator;
+public class OfferingList {
+	ArrayList<string> sup = new ArrayList<>();
+	
+	OfferingList()
+	{
+		sup.add("Offer for Tomato");
+		sup.add("Offer for Mutton");
+		sup.add("Offer for Onion");
+		sup.add("offer for Beef");
+		sup.add("offer for Pork");
+	}
 
 }
